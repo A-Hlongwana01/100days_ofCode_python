bf472a3e8705ef9600d0e3ec40d242776c2aad0b
ahlongwa021 <ahlongwa021@student.wethinkcode.co.za>
2022-07-05 01:14:57 +0200

    used a different method of printing the len of a string

--- day_1__string_manipulation/stdin.py
+++ day_1__string_manipulation/stdin.py
@@ -2,3 +2,6 @@
 fname = input("Enter your name: ")
 # printing out the length of the string stored in var 'fname' using the built-in function 'len()'
 print(len(fname))
+
+# same thing could've been done in the following way (in one line):
+print(len(input("Enter name: ")))
