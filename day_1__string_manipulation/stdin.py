ca809e28ca257ae86d846dfd5a39e692486c91f0
ahlongwa021 <ahlongwa021@student.wethinkcode.co.za>
2022-07-05 01:10:58 +0200

    used a var to store a string

---
+++ day_1__string_manipulation/stdin.py
@@ -0,0 +1,4 @@
+# Storing user input in variable 'fname'
+fname = input("Enter your name: ")
+# printing out the length of the string stored in var 'fname' using the built-in function 'len()'
+print(len(fname))
--- day_1__string_manipulation/string_man.py
+++ day_1__string_manipulation/string_man.py
@@ -1,4 +1,4 @@
 print("Day 1 - String manipulation")
 print('String concatenation is done with the "+" sign')
 print("e.g. print('Hellow '+'world')")
-print("new lines can be created using a backslash and n.")
+print("new lines can be created using a backslash and n.") 
