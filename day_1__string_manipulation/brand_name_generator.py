24d04140b57da99993ebbb33cd9d3de8f481fb85
ahlongwa021 <ahlongwa021@student.wethinkcode.co.za>
2022-07-05 12:05:31 +0200

    worked with variables

---
+++ day_1__string_manipulation/brand_name_generator.py
@@ -0,0 +1,7 @@
+from unicodedata import name
+
+
+print("Hello there!")
+name_of_city = input("What is the name of the city you grew up in? \n")
+pets_name = input("What is your pets name? \n")
+print("Band name is "+name_of_city+" "+pets_name)
