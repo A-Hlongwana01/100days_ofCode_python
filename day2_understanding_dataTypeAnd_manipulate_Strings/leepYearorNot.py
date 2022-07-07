bcc21c6714895db3e6b93426c9d37b104ac5ac51
ahlongwa021 <ahlongwa021@student.wethinkcode.co.za>
2022-07-07 22:38:47 +0200

    program complete: checks if a given year is leap year or not:)

---
+++ day2_understanding_dataTypeAnd_manipulate_Strings/leepYearorNot.py
@@ -0,0 +1,39 @@
+def yearInput():
+    year = int(input("Enter year you want to check: "))
+    return year
+
+
+def isLeapYear(year):
+    # if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    #     return "not a leap year"
+    # else: return "a leap year"\
+    if year % 4 != 0:
+        leapYear = False
+    else:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leapYear = True
+            else:
+                leapYear = False
+        else:
+            leapYear = True
+
+    return leapYear
+
+
+def result(isLeapYear)-> str:
+    if isLeapYear:
+        return "a leap year"
+    else:
+        return "not a leap year"
+        
+
+def printResult(year,result):
+    print("{} is {}".format(year, result))
+
+if __name__=="__main__":
+    yearTocheck = yearInput()
+    isLeap_orNot = isLeapYear(yearTocheck)
+    a = result(isLeap_orNot)
+    printResult(yearTocheck, a)
+
