8b1f29c8bdd5c7eeee3e0a3acfdf3179de95ec1b
ahlongwa021 <ahlongwa021@student.wethinkcode.co.za>
2022-07-07 21:12:46 +0200

    update: program should give a status to the user depending on their bmi result

--- day2_understanding_dataTypeAnd_manipulate_Strings/BMI_calc.py
+++ day2_understanding_dataTypeAnd_manipulate_Strings/BMI_calc.py
@@ -1,3 +1,12 @@
+# if BMI < 18.5 = underweght
+# if BMI > 18.5 and <25 = normal weight
+# between 25 and 30 = overweight
+# between 30 and 35 = obese
+# > 35 = critically obese
+
+from re import L
+
+
 def userInput():
     height = input("Enter your height(m): ")
     weight = input("Enter weight(kg): ")
@@ -23,10 +32,31 @@ def bmi(weight_, height_):
     return result
 
     
+def bmiStatus(bmi):
+
+    if bmi < 18.5:
+        status = "underweight"
+
+    elif bmi < 25:
+        status = "normal weight"
+
+    elif bmi < 30:
+        status = "overweight"
+
+    elif bmi < 35:
+        status = "obese"
+
+    else:
+        status =  "critically obese"
+
+    return status
+
+
 if __name__ == "__main__":
     input_ = userInput()
     height_ = height(input_)
     weight_ = weight(input_)
     result = bmi(weight_,height_)
-    print("Your body mass index (BMI) is: {}".format(round(result)))
+    weightStatus = bmiStatus(result)
+    print("Your body mass index (BMI) is: {} you are {}.".format(round(result), weightStatus))
     
